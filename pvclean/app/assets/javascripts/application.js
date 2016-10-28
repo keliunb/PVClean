1a524c6fc126c5d2c8fd7a928ffb49c0e0120ee6
Victor Fellipe <victorfgcabeceira@gmail.com>
2016-10-28 18:18:27 -0200

    Changing navbar

--- pvclean/app/assets/javascripts/application.js
+++ pvclean/app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+// Allow Bootstrap dropdown menus to have forms/checkboxes inside,
+// and when clicking on a dropdown item, the menu doesn't disappear.
+$(document).on('click', '.dropdown-menu.dropdown-menu-form', function(e) {
+  e.stopPropagation();
+});
