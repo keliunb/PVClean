deb0a6df3c35607712939bf09c9240a829920593
Victor <victorfgcabeceira@gmail.com>
2016-11-29 16:46:45 -0200

    Atualizando script de testes

--- ruby-server_simples.rb
+++ ruby-server_simples.rb
@@ -1,13 +1,43 @@
 #!/usr/bin/ruby
 require 'socket'
-server = TCPServer.new("", 6002)
-loop {
-  client = server.accept
-  mensagem_recebida = client.recv(100)
-    client.send("recebi " + mensagem_recebida, 0)
-    print mensagem_recebida
 
-  client.send("olaaaaaaaa",0)
-  #client.puts "PI2 :-)"
+#Abre o servidor TCP que será utilizado nos testes
+server = TCPServer.new("localhost", 8000)
+loop {
+  #aceite a conexão com o cliente
+  client = server.accept
+
+  #Mensagem recebida do cliente, de até 100 caracteres
+  mensagem_recebida = client.recv(100)
+
+  #Apresenta no terminal qual foi a mensagem recebida
+  if mensagem_recebida == "1_3"
+    print mensagem_recebida + ": Limpeza completa\n"
+    #Envia para o servidor a mensagem de sucesso
+    client.send("Servidor recebeu " + mensagem_recebida + "\n", 0)
+  elsif mensagem_recebida == "0_0"
+    print mensagem_recebida + ": Parada completa\n"
+    #Envia para o servidor a mensagem de sucesso
+    client.send("Servidor recebeu " + mensagem_recebida + "\n", 0)
+  elsif mensagem_recebida == "1_2"
+    print mensagem_recebida + ": Limpeza da primeira metade das placas\n"
+    #Envia para o servidor a mensagem de sucesso
+    client.send("Servidor recebeu " + mensagem_recebida + "\n", 0)
+  elsif mensagem_recebida == "2_3"
+    print mensagem_recebida + ": Limpeza da segunda metade das placas\n"
+    #Envia para o servidor a mensagem de sucesso
+    client.send("Servidor recebeu " + mensagem_recebida + "\n", 0)
+  end
+
+  #Tempo para sincronizar o envio da mensagem para o cliente
+  sleep 0.001
+  #Envia para o servidor a mensagem de sucesso
+  client.send("Servidor recebeu " + mensagem_recebida + "\n", 0)
+
+    #client.send("Servidor recebeu " + mensagem_recebida + "\n", 0)
+#Envia mensagem para o Cliente
+  #client.send("Teste de envio de mensagem - PI2",0)
+
+  #Fecha a conexão com o cliente
   client.close
 }
