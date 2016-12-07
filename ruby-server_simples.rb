ff2c698c5bd164453a7e5cc46a96719b7aa3b233
Victor <victorfgcabeceira@gmail.com>
2016-12-06 22:44:58 -0200

    Updating script

--- ruby-server_simples.rb
+++ ruby-server_simples.rb
@@ -15,7 +15,8 @@ loop {
     print mensagem_recebida + ": Limpeza completa\n"
     #Envia para o servidor a mensagem de sucesso
     #client.send("Servidor recebeu " + mensagem_recebida + "\n", 0)
-    client.send("12.123.456_19.91_1_0_1",0)
+    client.send("12.123.456_19.91_1_1_0",0)
+    #client.send("12.123.456_19.91_0_0_1",0)
   elsif mensagem_recebida == "0_0"
     print mensagem_recebida + ": Parada completa\n"
     #Envia para o servidor a mensagem de sucesso
@@ -23,11 +24,13 @@ loop {
   elsif mensagem_recebida == "0_1"
     print mensagem_recebida + ": Limpeza da primeira metade das placas\n"
     #Envia para o servidor a mensagem de sucesso
-    client.send("Servidor recebeu " + mensagem_recebida + "\n", 0)
+    #client.send("Servidor recebeu " + mensagem_recebida + "\n", 0)
+    client.send("30.123.456_39.91_0_0_0",0)
   elsif mensagem_recebida == "1_2"
     print mensagem_recebida + ": Limpeza da segunda metade das placas\n"
     #Envia para o servidor a mensagem de sucesso
-    client.send("Servidor recebeu " + mensagem_recebida + "\n", 0)
+    #client.send("Servidor recebeu " + mensagem_recebida + "\n", 0)
+    client.send("04.123.456_29.91_1_0_1",0)
   end
 
   #Tempo para sincronizar o envio da mensagem para o cliente
